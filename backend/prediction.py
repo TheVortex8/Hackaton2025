5f1e990d195ed9a0d603a2fe87171267a6f5b83c
jammy312 <james.brutus@polymtl.ca>
2025-02-01 16:52:21 -0500

    essaie quelque chose

--- backend/prediction.py
+++ backend/prediction.py
@@ -2,13 +2,27 @@ import numpy as np
 import pandas as pd
 from enum import Enum
 
-if __name__ == '__main__':
-    wildfire_history = pd.read_csv('backend/dataset/historical_wildfiredata.csv')
-    env_history = pd.read_csv('backend/dataset/historical_environmental_data.csv')
+
+severity = {'': 0, 'low': 1, 'medium': 2, 'high': 3}
+
+def get_categorize_serenity(wildfire_history,env_history):
+    severity_by_timestamp = np.array([])
     for env_entry in env_history:
         severity_level = ''
         for wildfire_entry in wildfire_history:
-                if(wildfire_entry['timestamp'] == env_entry['timestamp']):
-                    severity_level = wildfire_entry['severity']
+                if(wildfire_entry[0] == env_entry[0]):
+                    wildfire_severity = wildfire_entry[3]
+                    severity_level = severity[wildfire_severity]
                     break
+        np.append(severity_by_timestamp,severity_level )
+        
+    return severity_by_timestamp
+        
+
+if __name__ == '__main__':
+    wildfire_history = pd.read_csv('backend/dataset/historical_wildfiredata.csv')
+    env_history = pd.read_csv('backend/dataset/historical_environmental_data.csv')
+
+    severity_by_timestamp =  get_categorize_serenity(wildfire_history.values, env_history.values)
+    
             
