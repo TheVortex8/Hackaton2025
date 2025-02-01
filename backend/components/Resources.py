16924fe1cf1f5037553574e32bab6b8c29fb3175
Abdul Rahman Zahid <103886116+LLAbdul@users.noreply.github.com>
2025-02-01 14:31:33 -0500

    add resource constructor

---
+++ backend/components/Resources.py
@@ -0,0 +1,5 @@
+class Resources:
+    def __init__(self, deployment_time, dollar_by_operation,unit_available):
+        self.deployment_time = deployment_time
+        self.dollar_by_operation = dollar_by_operation
+        self.unit_available = unit_available
