0c03b9620451f00b85d521db8d59ae07abede286
Amine <mohamed-amine.elarabi.1@ens.etsmtl.ca>
2025-02-01 14:42:56 -0500

    added resources

--- backend/resources.py
+++ backend/resources.py
@@ -1,5 +1,6 @@
 class Resources:
-    def __init__(self, deployment_time, dollar_by_operation,unit_available):
+    def __init__(self, name, deployment_time, dollar_by_operation,unit_available):
+        self.name = name
         self.deployment_time = deployment_time
         self.dollar_by_operation = dollar_by_operation
         self.unit_available = unit_available
