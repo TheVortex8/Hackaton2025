fe31e0d5dd6be5d4b930789e90c01f029cd98dee
Amine <mohamed-amine.elarabi.1@ens.etsmtl.ca>
2025-02-01 18:48:51 -0500

    wip add miss cost

--- backend/optimize.py
+++ backend/optimize.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from resources import damage_costs_df
 
 def optimize(wildfires_df: pd.DataFrame, resources_df: pd.DataFrame): 
     wildfires_df.sort_values(by='severity', key=lambda x: x.map({'high': 1, 'medium': 2, 'low': 3}), inplace=True)
@@ -16,23 +17,34 @@ def optimize(wildfires_df: pd.DataFrame, resources_df: pd.DataFrame):
     resource_deployments = []
     current_resource = resources_df.iloc[0]
     for _, wildfire_row in wildfires_df.iterrows():
-        resource_deployments.append({
-            'reported_time': wildfire_row['timestamp'],
-            'estimated_fire_start_time': wildfire_row['fire_start_time'],
-            'severity': wildfire_row['severity'],
-            'location': wildfire_row['location'],
-            'assigned_resource': current_resource['name'],
-            'deployment_time': current_resource['time'],
-            'cost': current_resource['cost']
-        })
-        current_resource['units'] -= 1 
+        if current_resource is None:
+            resource_deployments.append({
+                'reported_time': wildfire_row['timestamp'],
+                'estimated_fire_start_time': wildfire_row['fire_start_time'],
+                'severity': wildfire_row['severity'],
+                'location': wildfire_row['location'],
+                'cost': current_resource['cost'],
+                'miss_cost': damage_costs_df['cost']
+            })
+        else:
+            resource_deployments.append({
+                'reported_time': wildfire_row['timestamp'],
+                'estimated_fire_start_time': wildfire_row['fire_start_time'],
+                'severity': wildfire_row['severity'],
+                'location': wildfire_row['location'],
+                'assigned_resource': current_resource['name'],
+                'deployment_time': current_resource['time'],
+                'cost': current_resource['cost'],
+                'miss_cost': damage_costs_df['cost']
+            })
+            current_resource['units'] -= 1 
         
         
         if current_resource['units'] == 0:
             next_resource_index = resources_df.index.get_loc(current_resource.name) + 1
             if next_resource_index >= len(resources_df): # No more resources to deploy
-                break 
-            
-            current_resource = resources_df.iloc[next_resource_index]
+                current_resource = None 
+            else:
+                current_resource = resources_df.iloc[next_resource_index]
 
     return pd.DataFrame(resource_deployments)
--- backend/resources.py
+++ backend/resources.py
@@ -2,3 +2,4 @@
 import pandas as pd
 
 resources_df = pd.read_csv('dataset/resources.csv')
+damage_costs_df = pd.read_csv('dataset/damage_costs.csv')
