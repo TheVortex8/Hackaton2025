259a4c0357d40f9de522d54f48ecc8c6b5abaef7
jammy312 <james.brutus@polymtl.ca>
2025-02-01 23:22:13 -0500

    YESSSS IA FONCTIONNE

--- backend/prediction.py
+++ backend/prediction.py
@@ -3,10 +3,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_squared_error
-
+import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
+import os
 
 severity_map = {'': 0, 'low': 1, 'medium': 2, 'high': 3}
-
 
 def get_environment_data(environment_history_df:pd.DataFrame) -> np.ndarray:
   return np.array([[environment_entry['temperature'],
@@ -32,31 +33,77 @@ def categorize_by_severity(wildfire_history_df: pd.DataFrame, environment_histor
       
   return severity_by_timestamp
 
-
-def initialize_model(environment_history_df_values: np.ndarray,severity_by_timestamp:np.ndarray[int]) -> RandomForestClassifier:
+def create_model(environment_history_df_values: np.ndarray,severity_by_timestamp:np.ndarray[int]) -> RandomForestClassifier:
   X = environment_history_df_values
   y = severity_by_timestamp
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+  X_train, X_test, y_train, y_test = train_test_split(X,y )
 
-  model = RandomForestClassifier(class_weight={0: 1, 1: 10, 2: 10, 3: 10}, random_state=42, min_samples_leaf=1, max_features='sqrt')
-  model.fit(X_train, y_train)
+  model = RandomForestClassifier(class_weight={0: 1, 1: 25, 2: 25, 3: 25}, random_state=42, min_samples_leaf=1, max_features='sqrt')
+  smote = SMOTE(sampling_strategy={1: 3500, 2: 3500, 3: 3500},  random_state=42)
+  new_X, new_Y = smote.fit_resample(X_train, y_train)
+  
+  model.fit(new_X, new_Y)
 
   y_pred = model.predict(X_test)
-
   mse = mean_squared_error(y_test, y_pred)
   print(f"Erreur quadratique moyenne (MSE) : {mse}")
 
   print("Prédictions : ", y_pred)
   return model
 
+def get_future_wildfire(model:RandomForestClassifier) -> pd.DataFrame:
+  future_environment_df = pd.read_csv('dataset/future_environmental_data.csv')
+  future_environment = get_environment_data(future_environment_df)
+  future_wildfire_severity =  model.predict(future_environment)
 
-if __name__ == '__main__':
+  future_wildfire = []
+  for index in range(len(future_wildfire_severity)):
+    severity = future_wildfire_severity[index]
+    if(severity == 0):
+      continue
+
+    latitude = future_environment_df["latitude"][index]
+    longitude = future_environment_df["longitude"][index]
+
+    location = f"{latitude},{longitude}"
+
+    future_wildfire.append({
+      "timestamp":future_environment_df["timestamp"][index],
+      "fire_start_time": future_environment_df["timestamp"][index],
+      "location": location,
+      "severity": severity,   
+    })
+  return pd.DataFrame(future_wildfire)
+
+def get_prediction() -> pd.DataFrame:
   wildfire_history_df = pd.read_csv('dataset/historical_wildfiredata.csv')
   environment_history_df = pd.read_csv('dataset/historical_environmental_data.csv')
 
   environment_data = get_environment_data(environment_history_df)
   severity_by_timestamp =  categorize_by_severity(wildfire_history_df, environment_history_df)
   
-  model = initialize_model(environment_data, severity_by_timestamp)
+  model = create_model(environment_data, severity_by_timestamp)
+
+  future_wildfire = get_future_wildfire(model)
+
+  os.makedirs('generated', exist_ok=True)
+  future_wildfire.to_csv("generated/predictions.csv", index=False, encoding="utf-8")
+
+  return future_wildfire
+
+
+
+get_prediction()
+
+
+
+
+
+
+  
+
+
+
+   
     
             
